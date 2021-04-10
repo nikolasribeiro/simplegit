f839b1d5940e53d68d9e04d9a7355db7f1433c39
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-04-10 02:18:07 -0300

    first commit from script

---
+++ simplegit.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+/**
+ * SimpleGit is a simple layer to use git on Linux
+ * Author: Nicolas Ribeiro
+ */
+const commands = require('./utils/commands');
+const process = require('process');
+
+/*
+Splice method set the start of the list, in this case, just ommit the first 2 elements
+from terminal arguments
+*/
+const arguments = process.argv.splice(2);
+const FLAGS = {
+    '-a': commands.add,
+    '-s': commands.status,
+    '-h': commands.help
+}
+
+
+// Checks if user puts arguments
+if(arguments.length !== 0){
+    
+    // Loop over every argument get from terminal
+    arguments.forEach((arg, index) => {
+        
+        if(arg.includes('-') || arg.includes('--')){
+
+            switch (true) {
+
+                case (arg.includes('h') || arg.includes('help')):
+                    commands.help();
+                    break;
+
+                case (arg.includes('s') || arg.includes('status')):
+                    commands.status();
+                    break;
+
+                case (arg.includes('a') || arg.includes('add')):
+
+                    // checks if the flags with double dash exist
+                    if (arguments[index].includes('--all')){
+                        commands.add('all');
+                        break;
+                    }
+                    else if (arguments[index+1].includes('--file')){
+                        commands.add(arguments[index+2]);
+                        break;
+                    }
+                    break;
+
+                case (arg.includes('c') ||arg.includes('commit')):
+                    
+                    if(arguments[index+1] === undefined){
+                        commands.commit('commit');
+                    }else{
+                        commands.commit(arguments[index+1]);
+                    }
+                    break;
+                
+                case (arg.includes('p') ||arg.includes('push')):
+                    commands.push('commit');
+                    break;
+            }
+        }
+    });
+
+}else{
+    commands.status();
+    console.log('if you want to know the commands, you can use: simplegit -h');
+}
+
+
---
+++ utils/commands.js
@@ -0,0 +1,54 @@
+const child_process = require('child_process');
+
+// function that execute the command
+function executeCommand(command){
+    child_process.exec(command, (err, stderr, stdout)=>{
+        // Checks if the commands fail
+        if(err){
+            console.log(`Error: ${err.message}`);
+            return;
+        }
+        // Prints the standar err
+        if(stderr){
+            console.log(`${stderr}`);
+        }
+        console.log(stdout);
+    });
+}
+
+
+
+module.exports = {
+
+    status  : () => { executeCommand('git status') },
+
+    help    : () => {
+        console.log(`
+            ====== Simplegit ======
+            how to use it:
+            $ simplegit [flags[...]]
+            FLAGS:
+            -h, --help    |--> help, shows the info about how the script works
+            -s, --status  |--> status, shows the git status of the current repository
+        `);
+    },
+
+    add     : (file) => { 
+        if(file !== 'all'){
+            executeCommand(`git add ${file}`);
+            
+        }else{
+            executeCommand(`git add -A`);   
+        }
+    },
+
+    commit  : (message) => { 
+        if (message !== 'commit'){
+            executeCommand(`git commit -m "${message}"`);
+        }else{
+            executeCommand('git commit -m "commit"');
+        }
+    },
+
+    push    : () => { executeCommand('git push'); }
+}
