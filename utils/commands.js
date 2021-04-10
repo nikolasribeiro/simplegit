dbe66c001dc2f9d98acdf565ba48b7cac3a60795
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-04-10 02:58:32 -0300

    adding help of DOITALL

--- utils/commands.js
+++ utils/commands.js
@@ -25,19 +25,30 @@ module.exports = {
             ====== Simplegit ======
             how to use it:
             $ simplegit [flags[...]]
+
             FLAGS:
             -h, --help      |--> help, shows the info about how the script works
+            
             -s, --status    |--> status, shows the git status of the current repository
+            
             -a, --add       |--> add, Adds the current changes to commit later, is necessary to use
                                  --all if you want to add for commit all the changes or --file if 
                                  you want to add only one file for commit.
             -c, --commit    |--> commit, Commit all the changes that you previously added
+            
             -p, --push      |--> push, Push all the commits to the remote repository
+            
             -l, --log       |--> Log, shows the history of changes and commits in a nice looking way,
                                  also you get the commit ID if you want to checkout to a specific commit
             -Ch,--Checkout  |--> Checkout, move to another branch or previous commit
+            
+            -DOITALL        |--> DOITALL, custom command that mades for you git status, git add -A, git commit and git push.
+                                 if you want a specific commit message, you need to do: 
+                                    $ simplegit -DOITALL "YOUR COMMIT MESSAGE HERE"
+                                 if you dont put a specific commit message, by default, the commit message
+                                 will be 'commit'
         `);
-    },
+    }, 
     add      : (file) => { 
         if(file !== 'all'){
             executeCommand(`git add ${file}`);
